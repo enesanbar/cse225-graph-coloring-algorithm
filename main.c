414034ae6fef0b27a38e3ecb7c78bfe8f8e7a547
Enes Anbar <enesanbar@gmail.com>
2014-12-05 16:46:50 +0200

    .gitignore file added

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
-/*
-CSE225 - Data Structures Project 3
-Enes ANBAR - 150113842
-*/
-
+/*  CSE225 - Data Structures Project 3
+    Enes ANBAR - 150113842              */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
 
-
+	printf("%s\n", "It works!");
 
     return 0;
 }
